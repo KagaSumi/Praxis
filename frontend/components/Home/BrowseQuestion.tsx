2f65b2cfc77c9649c395b2769a8454fbfca59f66
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2025-11-01 22:30:41 -0700

    implement tag filter

--- frontend/components/Home/BrowseQuestion.tsx
+++ frontend/components/Home/BrowseQuestion.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+
+// models
+import { Tag, TagModel } from "../../model/Tag";
+import { Question } from "../../model/QuestionModel";
 
 // components
 import Sidebar from "../Sidebar";
@@ -7,11 +11,31 @@ import ViewPostCard from "../ViewPostCard";
 import Card from "../Card/Card";
 import PillButton from "../Card/PillButton";
 
-//model
-import { Question, QuestionModel } from "../../model/QuestionModel";
-import { Tag, TagModel } from "../../model/Tag";
+export default function BrowseQuestion({
+  tags,
+  questions,
+}: {
+  tags: Array<Tag>;
+  questions: Array<Question>;
+}) {
+  const [appliedTags, setAppliedTags] = useState<Set<string>>(new Set());
 
-export default function BrowseQuestion({ tags, questions }) {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const selectedTags = data.getAll("tags") as string[];
+    console.log(selectedTags);
+    setAppliedTags(new Set(selectedTags));
+  }
+
+  const filteredQuestions = useMemo(() => {
+    if (appliedTags.size === 0) return questions;
+
+    return questions.filter((question) =>
+      question.tags?.some((tag) => appliedTags.has(tag)),
+    );
+  }, [questions, appliedTags]);
+
   return (
     <div className="grid gap-6 grid-cols-[260px_1fr]">
       {/* Left Sidebar */}
@@ -20,28 +44,35 @@ export default function BrowseQuestion({ tags, questions }) {
           <div className="mb-3 text-md font-semibold text-slate-900">
             Filter
           </div>
-          <ul className="space-y-2 text-sm h-[150px] overflow-hidden">
-            {tags.map((tag) => (
-              <li key={tag.tag_id}>
-                <label className="inline-flex cursor-pointer items-center gap-2">
-                  <input
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-slate-300 text-blue-600 focus:ring-blue-600"
-                  />
-                  <span className="text-slate-700">{tag.name}</span>
-                </label>
-              </li>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-2 text-sm  overflow-hidden"
+          >
+            {/*max-h-[150px]*/}
+            {tags.map((tag: Tag) => (
+              <label
+                key={tag.tag_id}
+                className="inline-flex cursor-pointer items-center gap-2"
+              >
+                <input
+                  type="checkbox"
+                  name="tags"
+                  value={tag.name}
+                  className="h-4 w-4 rounded border-slate-300 text-blue-600 focus:ring-blue-600"
+                />
+                <span className="text-slate-700">{tag.name}</span>
+              </label>
             ))}
-          </ul>
-          <div className="mt-3">
-            <p className="text-slate-700 text-sm underline cursor-pointer">
-              Show more filters
-            </p>
-          </div>
+            <div className="mt-3">
+              <p className="text-slate-700 text-sm underline cursor-pointer">
+                Show more filters
+              </p>
+            </div>
 
-          <div className="mt-4">
-            <PillButton>Apply</PillButton>
-          </div>
+            <div className="mt-4">
+              <PillButton type="submit">Apply</PillButton>
+            </div>
+          </form>
         </Card>
       </Sidebar>
 
@@ -82,7 +113,7 @@ export default function BrowseQuestion({ tags, questions }) {
               Newest Questions
             </h2>
             <div className="p-1 flex flex-col gap-5">
-              {questions.map((q) => (
+              {filteredQuestions.map((q) => (
                 <ViewPostCard
                   key={q.questionId}
                   questionId={q.questionId}
