869e330c8047bf724d910ae8577fad080c867a62
taylor.ockerman <taylor.ockerman@gmail.com>
2025-11-19 11:51:38 -0800

    updated vitest.config to hopefully prevent commonjs usage

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
         environment: 'jsdom',
         globals: true,
         setupFiles: './test/setupTests.ts',
+        // might fix issues with common js being used by some next internals
+        deps: ({
+            inline: ['next', 'next/dist/*']
+        } as any),
         coverage: {
             provider: 'v8',
             reporter: ['text', 'lcov'],
